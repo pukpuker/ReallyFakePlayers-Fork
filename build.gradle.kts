1a9281ec9b75186dd0da0f0e7c3b24addb79a724
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-05-12 14:19:15 +0200

    ein paar Abhängigkeiten updaten

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ dependencies {
     paperweight.paperDevBundle("1.20.5-R0.1-SNAPSHOT")
 
     compileOnly("org.jetbrains:annotations:24.1.0")
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
 
     compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.2.3")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
@@ -31,12 +31,12 @@ dependencies {
     paperLibrary("com.google.inject:guice:7.0.0")
     paperLibrary("com.google.inject.extensions:guice-assistedinject:7.0.0")
     paperLibrary("org.javassist:javassist:3.30.2-GA")
-    paperLibrary("net.bytebuddy:byte-buddy-agent:1.14.12")
+    paperLibrary("net.bytebuddy:byte-buddy-agent:1.14.15")
     paperLibrary("com.github.steveice10:MCProtocolLib:6214f75699")
     paperLibrary("dev.dejvokep:boosted-yaml-spigot:1.4")
-    paperLibrary("xyz.jpenilla:reflection-remapper:0.1.0")
-    paperLibrary("com.github.Revxrsal.Lamp:common:3.1.9")
-    paperLibrary("com.github.Revxrsal.Lamp:bukkit:3.1.9")
+    paperLibrary("xyz.jpenilla:reflection-remapper:0.1.1")
+    paperLibrary("com.github.Revxrsal.Lamp:common:3.2.1")
+    paperLibrary("com.github.Revxrsal.Lamp:bukkit:3.2.1")
 }
 
 paper {
